787d2dbafb03667ae6b764c1fe77cafaeae529d7
SeongChan Lee <foriequal@gmail.com>
2018-04-02 20:59:00 +0900

    Transform::setOrientation 에 euler angle 인터페이스 추가

--- Transform.hpp
+++ Transform.hpp
@@ -21,13 +21,24 @@ public:
     return *this;
   }
 
-  inline Transform &setPosition(float x, float y, float z) {
+  inline Transform& setPosition(float x, float y, float z) {
     position = {x, y, z};
     return *this;
   }
 
-  inline Transform &setOrientation(float w, float x, float y, float z) {
-    orientation = {w, x, y, z};
+  inline Transform& setOrientation(float w, float x, float y, float z) {
+    orientation = Eigen::Quaternionf{w, x, y, z}.normalized();
+    return *this;
+  }
+
+#define radianf(x) ((float)(M_PI/180.0f*x))
+  inline Transform &setOrientation(float x, float y, float z) {
+    auto radX = radianf(x/2);
+    auto radY = radianf(y/2);
+    auto radZ = radianf(z/2);
+    orientation = Eigen::Quaternionf{cosf(radX), sinf(radX), 0, 0}
+      * Eigen::Quaternionf{cosf(radY), 0, sinf(radY), 0}
+      * Eigen::Quaternionf{cosf(radZ), 0, 0, sinf(radZ)};
     return *this;
   }
 };
